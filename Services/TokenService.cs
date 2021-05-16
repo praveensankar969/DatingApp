0ee3f61b5b7af92c190cc8db3890d0c2dba9c215
praveensankar969 <sankar_praveen@hotmail.com>
2021-05-16 10:35:18 +0530

    Token generation

---
+++ Services/TokenService.cs
@@ -0,0 +1,15 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services{
+
+    public class TokenService : ITokenInterface{
+        public TokenService()
+        {
+        }
+
+        public string CreateToken(AppUser user){
+            
+        }
+    }
+}
